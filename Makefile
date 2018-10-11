2e0abd4ab3152ac8872da1a422b5595a0a097ed1
Cristobal Garcia <cristobal.garcia@obliquo.eu>
2018-10-11 10:48:30 +0200

    Enhance makefile

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 .PHONY: test
 
-dist/c1: dist
+dist/c1: $(wildcard ./cmd/c1/*.go) dist
 	@go build -o dist/c1 ./cmd/c1
 
 dist:
--- cmd/c1/main.go
+++ cmd/c1/main.go
@@ -2,8 +2,8 @@ package main
 
 import "fmt"
 
-
 func main() {
-    fmt.Println("About to have an API server")
-    fmt.Println("---------------------------")
+	fmt.Println("---------------------------")
+	fmt.Println("About to have an API server")
+	fmt.Println("---------------------------")
 }
